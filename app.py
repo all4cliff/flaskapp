84de123f582df5b96007dcba66ee72b6dea02049
all4cliff <124750514+all4cliff@users.noreply.github.com>
2023-03-30 16:42:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+#!/bin/python
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "I am almost a Devops Engineer!"
+
+if __name__ == "__main__":
+ app.run(host='0.0.0.0')
